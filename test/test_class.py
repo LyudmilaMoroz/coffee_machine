7004f01b6e885cda824bf0c3edcf7e9725c1177b
Liisa <liisa@gmail.com>
2022-08-11 13:45:17 +0300

    Add a fifth, sixth and seventh test cases

--- test/test_class.py
+++ test/test_class.py
@@ -1,6 +1,13 @@
 class TestMaster:
 
-    def test_four_case(self):
+    def test_case_four(self):
         pass
 
+    def test_case_five(self):
+        pass
 
+    def test_case_six(self):
+        pass
+
+    def test_case_seven(self):
+        pass
